739a6aea558b86e5e16cb66b838886d449c8fd0d
Bruno Urbano Rodrigues <brunourb@gmail.com>
2023-03-10 19:45:06 -0300

    feature(arch): projeto inicial - commit inicial

---
+++ src/main/java/br/senac/go/App.java
@@ -0,0 +1,8 @@
+package br.senac.go;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
---
+++ src/test/java/br/senac/go/testing/AppTest.java
@@ -0,0 +1,12 @@
+package br.senac.go.testing;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		assert Boolean.TRUE;
+	}
+	
+}
