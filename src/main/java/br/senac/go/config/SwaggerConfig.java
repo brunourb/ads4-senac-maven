de7b03686715a8679b360d38099e48db9a9d3ed7
Bruno Urbano Rodrigues <brunourb@gmail.com>
2023-05-03 21:56:01 -0300

    feature(swagger): inserida configuração do swagger

---
+++ src/main/java/br/senac/go/config/SwaggerConfig.java
@@ -0,0 +1,56 @@
+package br.senac.go.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Optional;
+
+@Log4j2
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false)//
+                //.securitySchemes(new ArrayList<>(Arrays.asList(apiKey())))//
+//                .tags(new Tag("users", "Operations about users"))//
+//                .tags(new Tag("ping", "Just a ping"))//
+                .genericModelSubstitutes(Optional.class);
+    }
+
+    private ApiInfo apiInfo() {
+        log.trace("Retornando Builder de Informações da API");
+        return new ApiInfoBuilder().title("Desenvolvimento de web services")
+                .title("API - E-commerce")
+                .description("API baseada no OpenAPI(Swagger), Spring Boot 2.x.x.RELEASE, JWT, Swagger-ui 2.9.x, Postgresql, Java 8 e Lombok")
+                .termsOfServiceUrl("http://springfox.io")
+                .contact(new Contact("Faculdade SENAC"
+                        , "https://go.senac.br"
+                        , "email@email.com"))
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://github.com/springfox/springfox/blob/master/LICENSE")
+                .version("1.0.0")
+                .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Bearer %token", "Authorization", "Header");
+    }
+}
--- src/main/java/br/senac/go/resources/PessoaResource.java
+++ src/main/java/br/senac/go/resources/PessoaResource.java
@@ -2,13 +2,14 @@ package br.senac.go.resources;
 
 import br.senac.go.domain.Pessoa;
 import br.senac.go.generics.GenericOperationsResource;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.MediaType;
 import org.springframework.javapoet.ClassName;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.logging.Level;
@@ -16,6 +17,8 @@ import java.util.logging.Logger;
 
 @RestController //Fala que o controlador vai trabalhar com REST
 @RequestMapping(path = "/pessoa")
+@Api(value="Operações para manipulação dos dados do pessoa",
+        tags = "pessoa ")
 public class PessoaResource implements
         GenericOperationsResource<Pessoa, Integer> {
 
@@ -33,6 +36,17 @@ public class PessoaResource implements
             "application/xml;charset=UTF-8"},
     produces = {MediaType.APPLICATION_JSON_VALUE,
     MediaType.APPLICATION_XML_VALUE})
+    @ApiOperation(value="${resource.pessoa-post}",
+            notes="Criar dados de pessoa.")
+    @ApiResponses(value={
+            @ApiResponse(code = 200, message = "Requisição feita com sucesso.", response = Pessoa.class),
+            @ApiResponse(code = 201, message = "Registro criado com sucesso.", response = Pessoa.class),
+            @ApiResponse(code = 204, message = "O servidor processou a solicitação com sucesso e não está retornando nenhum conteúdo.", response = Pessoa.class),
+            @ApiResponse(code = 301, message = "Redirecionamento permanente.", response = Pessoa.class),
+            @ApiResponse(code = 401, message = "Não autorizado.", response = Pessoa.class),
+            @ApiResponse(code = 404, message = "Registro não encontrado.", response = Pessoa.class),
+            @ApiResponse(code = 500, message = "Erro na requisão, verifique configurações do servidor.", response = Pessoa.class)
+    })
     public Pessoa post(@RequestBody @Validated Pessoa entity) {
 
         LOGGER.log(Level.INFO,"Exemplo de imprimir", entity);
@@ -41,27 +55,45 @@ public class PessoaResource implements
     }
 
     @Override
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE})
     public List<Pessoa> get() {
+
+        LOGGER.log(Level.INFO,"Exemplo do GET:");
         return null;
     }
 
     @Override
-    public void put(Pessoa entity, Integer id) {
+    @PutMapping(value = "/{id}",
+            consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public void put(@RequestBody @Validated Pessoa entity,
+                    @PathVariable("id") Integer id) {
+        LOGGER.log(Level.INFO,
+                String.format("Exemplo do PUT: %s | %d", entity, id));
 
     }
 
     @Override
-    public void patch(Pessoa entity, Integer id) {
+    @PatchMapping(value = "/{id}",
+            consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public void patch(@RequestBody @Validated Pessoa entity,
+                      @PathVariable("id") Integer id) {
+        LOGGER.log(Level.INFO,
+                String.format("Exemplo do PATCH: %s | %d", entity, id));
 
     }
 
     @Override
-    public void delete(Pessoa entity) {
-
+    @DeleteMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public void delete(@RequestBody @Validated Pessoa entity) {
+        LOGGER.log(Level.INFO,
+                String.format("Exemplo do DELETE: %s", entity));
     }
 
     @Override
-    public void deleteById(Integer id) {
-
+    @DeleteMapping(value="/{id}")
+    public void deleteById(@PathVariable("id") Integer id) {
+        LOGGER.log(Level.INFO,
+                String.format("Exemplo do DELETE: %d", id));
     }
 }
